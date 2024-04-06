4c7c6a35b9c220fccdc3d0b16196031bcf12beb8
Paritosh Sharma <paritoshsharma0707@gmail.com>
2024-04-06 19:11:04 +0530

    implemented simple chatbot interface

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Llamma2-on-CPU"
+AUTHOR_USER_NAME = "Paritosh.Sharma"
+SRC_REPO = "LlammaInferencing_CPU"
+AUTHOR_EMAIL = "paritoshsharma0707@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "LlammaInferencing_CPU"},
+    packages=setuptools.find_packages(where="LlammaInferencing_CPU"),
+    install_requires=get_requirements("requirements.txt")
+)
